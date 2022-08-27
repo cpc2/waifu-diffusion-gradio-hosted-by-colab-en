d33d2bda196a54e2f4ae81c4493679f83bddf021
Miklós Nagy <miklos.mnagy@gmail.com>
2022-08-28 01:21:34 +0200

    Update kdiff_beta_en.py

--- scripts/kdiff_beta_en.py
+++ scripts/kdiff_beta_en.py
@@ -187,10 +187,10 @@ def split_weighted_subprompts(text):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--config", default="./configs/stable-diffusion/v1-inference.yaml", type=str)
-parser.add_argument("--ckpt", default="./models/ldm/stable-diffusion-v1/model.ckpt", type=str)
+parser.add_argument("--ckpt", default="/gdrive/MyDrive/model.ckpt", type=str)
 parser.add_argument("--precision", default="autocast", type=str)
 parser.add_argument("--outdir", default="./outputs/txt2img-samples", type=str)
-parser.add_argument("--GFPGAN", default='./models/ldm/stable-diffusion-v1/GFPGANv1.3.pth', type=str)
+parser.add_argument("--GFPGAN", default='/content/GFPGAN/experiments/pretrained_models/GFPGANv1.3.pth', type=str)
 
 args = parser.parse_args()
 
